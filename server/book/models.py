e6a9cbbd332fd1998efe1fd0f4dea4d0df04542b
vikrant-jadhav-github <149787240+vikrant-jadhav-github@users.noreply.github.com>
2023-12-04 16:20:57 +0530

    Removed models and serializers for book service

--- server/book/models.py
+++ server/book/models.py
@@ -1,19 +1,4 @@
 from django.db import models
-
-class Book(models.Model):
-    seller = models.ForeignKey('account.Seller', on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
-    author = models.CharField(max_length=100)
-    price = models.IntegerField()
-    cover = models.FileField(upload_to='book_covers/')
-    totalsold = models.IntegerField()
-    totalavailable = models.IntegerField()
-    genre = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return str(self.id)
     
 class Invoice(models.Model):
     order = models.OneToOneField('account.Order', on_delete=models.CASCADE)
